8d9ff58fbc0eeac93fabd99d4fdf34b6f2d734f5
Lukas Lerch <lukaslerch66@web.de>
2026-01-06 14:52:47 +0100

    Refactoring the code

--- src/convert.py
+++ src/convert.py
@@ -22,19 +22,18 @@ def slugify(name):
 
 def row_to_record(row):
     return {
-        "id": int(row[0]),
-        "name": normalize_spaces(row[1]),
-        "email": normalize_emails(row[2]),
-        "city": normalize_spaces(row[3]),
-        "slug": slugify(row[1])
+        "id": int(row["id"]),
+        "name": normalize_spaces(row["full_name"]),
+        "email": normalize_emails(row["email"]),
+        "city": normalize_spaces(row["city"]),
+        "slug": slugify(row["full_name"])
     }
 
 def convert_csv_to_json(in_path, out_path):
-    with open(in_path, "r") as f:
-        next(f)
-        rows = csv.reader(f)
+    with open(in_path, "r", newline="") as f:
+        rows = csv.DictReader(f)
         rows_to_json = [row_to_record(row) for row in rows]
         with open(out_path, "w") as j:
-            json.dump(rows_to_json, j)
+            json.dump(rows_to_json, j, ensure_ascii=False, indent=2)
 
 convert_csv_to_json(pathlib.Path("data/input.csv"),pathlib.Path("out/output.json"))
